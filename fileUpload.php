fdd8d0e6162465c642a0ee2e2a737cedbc26476c
Debosmit Ray <dray92@uw.edu>
2015-06-25 16:18:25 -0700

    test code

--- fileUpload.php
+++ fileUpload.php
@@ -1,5 +1,12 @@
 <?php
  
+ $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+ 
 // Path to move uploaded files
 $target_path = "uploads/";
  
